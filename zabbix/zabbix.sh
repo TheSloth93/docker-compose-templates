ccfe51cab781687d2b1800627904c23090d06aaa
TheSloth <kai-waggeling@web.de>
2022-08-18 23:33:20 +0200

    Update zabbix.sh

--- zabbix/zabbix.sh
+++ zabbix/zabbix.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/TheSloth93/docker-compose-templates/main/zabbix/zabbix.yaml
-wget https://raw.githubusercontent.com/TheSloth93/docker-compose-templates/main/zabbix/zabbix.env
+wget -q https://raw.githubusercontent.com/TheSloth93/docker-compose-templates/main/zabbix/zabbix.yaml
+wget -q https://raw.githubusercontent.com/TheSloth93/docker-compose-templates/main/zabbix/zabbix.env
 
-docker-compose --env-file ./zabbix.env -f ./zabbix.yaml -p zabbix -d up
+docker-compose -f ./zabbix.yaml -p zabbix up -d
+
+rm ./zabbix.yaml
+rm ./zabbix.env
