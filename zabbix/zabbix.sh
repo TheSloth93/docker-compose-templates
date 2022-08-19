8238982aca05836a4e6daab2bf5a802c0b5e9981
TheSloth <kai-waggeling@web.de>
2022-08-19 23:46:00 +0200

    Update zabbix.sh

--- zabbix/zabbix.sh
+++ zabbix/zabbix.sh
@@ -2,6 +2,6 @@
 
 wget -q https://raw.githubusercontent.com/TheSloth93/docker-compose-templates/main/zabbix/zabbix.yaml
 
-docker-compose -f ./zabbix.yaml -p zabbix up -d
+docker-compose -f zabbix.yaml -p zabbix up -d
 
 rm ./zabbix.yaml
