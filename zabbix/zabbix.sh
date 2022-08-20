f3aaf378456a17062b1010d8b585c3f9d7d4c2f5
TheSloth <kai-waggeling@web.de>
2022-08-20 14:04:15 +0200

    Update zabbix.sh

--- zabbix/zabbix.sh
+++ zabbix/zabbix.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
 
+echo "#############################################"
+echo "                                             "
+echo "        Project:    Zabbix Server            "
+echo "                                             "
+echo "                                             "
+echo "    please select your action:               "
+echo "                                             "
+
+select action in up down start stop
+echo "                                             "
+echo "    downloading scripts...                   "
+
+wget -q https://raw.githubusercontent.com/TheSloth93/docker-compose-templates/main/zabbix/zabbix.env
 wget -q https://raw.githubusercontent.com/TheSloth93/docker-compose-templates/main/zabbix/zabbix.yaml
 
-docker-compose -f zabbix.yaml -p zabbix up -d
+echo "    finished.                                "
+echo "                                             "
+echo "    executing docker-compose...              "
+
+docker-compose -f zabbix.yaml --env-file zabbix.env -p zabbix $action -d 1> /dev/null
+
+echo "    finished.                                "
+echo "                                             "
+echo "    deleting scripts                         "
 
 rm ./zabbix.yaml
+rm ./zabbix.env
+
+echo "    finished.                                "
+echo "                                             "
+echo "#############################################"
+echo ""
