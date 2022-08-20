b12d8a4ef2f2ae566e2b552c67697f75607ba7bf
TheSloth <kai-waggeling@web.de>
2022-08-20 14:07:58 +0200

    Update zabbix.sh

--- zabbix/zabbix.sh
+++ zabbix/zabbix.sh
@@ -9,26 +9,29 @@ echo "    please select your action:               "
 echo "                                             "
 
 select action in up down start stop
-echo "                                             "
-echo "    downloading scripts...                   "
+do
+  echo "                                             "
+  echo "    downloading scripts...                   "
 
-wget -q https://raw.githubusercontent.com/TheSloth93/docker-compose-templates/main/zabbix/zabbix.env
-wget -q https://raw.githubusercontent.com/TheSloth93/docker-compose-templates/main/zabbix/zabbix.yaml
+  wget -q https://raw.githubusercontent.com/TheSloth93/docker-compose-templates/main/zabbix/zabbix.env
+  wget -q https://raw.githubusercontent.com/TheSloth93/docker-compose-templates/main/zabbix/zabbix.yaml
 
-echo "    finished.                                "
-echo "                                             "
-echo "    executing docker-compose...              "
+  echo "    finished.                                "
+  echo "                                             "
+  echo "    executing docker-compose...              "
 
-docker-compose -f zabbix.yaml --env-file zabbix.env -p zabbix $action -d 1> /dev/null
+  docker-compose -f zabbix.yaml --env-file zabbix.env -p zabbix $action -d 1> /dev/null
 
-echo "    finished.                                "
-echo "                                             "
-echo "    deleting scripts                         "
+  echo "    finished.                                "
+  echo "                                             "
+  echo "    deleting scripts                         "
 
-rm ./zabbix.yaml
-rm ./zabbix.env
+  rm ./zabbix.yaml
+  rm ./zabbix.env
 
-echo "    finished.                                "
+  echo "    finished.                                "
+done
+
 echo "                                             "
 echo "#############################################"
 echo ""
